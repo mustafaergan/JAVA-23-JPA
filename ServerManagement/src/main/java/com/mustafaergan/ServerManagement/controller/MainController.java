7f33b74465a4ca373d9df95183299b4d44dde6b8
MustafaHoca <MustafaHoca@lab4-ogr>
2017-08-20 13:32:32 +0300

    spring boot

---
+++ ServerManagement/src/main/java/com/mustafaergan/ServerManagement/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.mustafaergan.ServerManagement.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path = "main")
+public class MainController {
+
+	@GetMapping(path = "hello")
+	@ResponseBody
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	
+	
+}
